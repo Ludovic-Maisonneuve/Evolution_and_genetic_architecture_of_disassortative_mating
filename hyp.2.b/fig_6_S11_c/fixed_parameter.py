c0824c19bf6ac8a5dab14b88bdc97af140433560
Ludovic-Maisonneuve <66014130+Ludovic-Maisonneuve@users.noreply.github.com>
2020-05-27 17:28:23 +0200

    Create fixed_parameter.py

---
+++ hyp.2.b/fig_6_S11_c/fixed_parameter.py
@@ -0,0 +1,10 @@
+#This file contains the value of the parameters that I don't varry in my study
+
+d_nm = 0.15 # basic predation mortality factor for individual not displaying the local mimetic signal
+d_m = 0.05 # basic predation mortality factor for individual not displaying the local mimetic signal
+delta = 0.1 #baseline mortality rate
+r = 1 # Growth rate
+K = 2000 # Carrying capacity
+lmbda = 0.0002 # Unpalatability coefficient
+N1 = 100 # Initial density of population in population 1
+N2 = 100 # Initial density of population in population 2
